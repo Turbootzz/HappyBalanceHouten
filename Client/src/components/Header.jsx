6d10c790a56f6eb61fb1fab57a0e83e3e79479ac
Thijs Herman <tc.herman@hotmail.com>
2024-10-24 22:48:01 +0200

    Added header boilerplate

---
+++ Client/src/components/Header.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { phone } from "../config/config";
+
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <nav className="bg-white border-gray-200 dark:bg-gray-900">
+          <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
+            <a
+              href="../pages/Home.jsx"
+              className="flex items-center space-x-3 rtl:space-x-reverse"
+            >
+              <img
+                src="https://flowbite.com/docs/images/logo.svg"
+                className="h-8"
+                alt="HappyBalance Logo"
+              />
+              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+                Logo
+              </span>
+            </a>
+            <div className="flex items-center space-x-6 rtl:space-x-reverse">
+              <a
+                href={"tel:" + phone}
+                className="text-sm  text-gray-500 dark:text-white hover:underline"
+              >
+                {phone}
+              </a>
+            </div>
+          </div>
+        </nav>
+        <nav className="bg-gray-50 dark:bg-gray-700">
+          <div className="max-w-screen-xl px-4 py-3 mx-auto">
+            <div className="flex items-center">
+              <ul className="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
+                <li>
+                  <a
+                    href="../pages/Home.jsx"
+                    className="text-gray-900 dark:text-white hover:underline"
+                    aria-current="page"
+                  >
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="../pages/Contact.jsx"
+                    className="text-gray-900 dark:text-white hover:underline"
+                  >
+                    Contact
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="../pages/terms.jsx"
+                    className="text-gray-900 dark:text-white hover:underline"
+                  >
+                    Voorwaarden
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </nav>
+    </header>
+  );
+}
