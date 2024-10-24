87ce5b6088c425571b4047cf1643824e43819108
Thijs Herman <tc.herman@hotmail.com>
2024-10-24 17:33:31 +0200

    Base Setup v2

--- Client/src/App.js
+++ Client/src/App.js
@@ -6,8 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1 class="text-3xl font-bold underline">Tailwind Test</h1>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
