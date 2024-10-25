a62b745b79036a2d9475fbbac7779652c191bf24
Thijs Herman <tc.herman@hotmail.com>
2024-10-25 16:54:42 +0200

    colours setup

--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <header>
       <nav>
-        <nav className="bg-white border-gray-200 dark:bg-gray-900">
+        <nav className="bg-mainbg border-gray-200 dark:bg-gray-900">
           <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
             <a
               href="../pages/Home.jsx"
@@ -17,7 +17,7 @@ export default function Header() {
                 alt="HappyBalance Logo"
               />
               <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-                Logo
+                Happy Balance
               </span>
             </a>
             <div className="flex items-center space-x-6 rtl:space-x-reverse">
--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        mainbg: "#FFCF9D",
+      },
+      colors: {
+        main: "#FFB38E",
+        mainlight: "#FFCF9D",
+        maindark: "#FFB26F",
+        maindarker: "#DE8F5F",
+      },
+    },
   },
   plugins: [],
-}
-
+};
