a5d7e6feeec4e932c85a6a468ed8a4231c69e181
Thijs Herman <tc.herman@hotmail.com>
2024-11-13 23:00:42 +0100

    updated for deployment

--- Client/src/components/ContactForm.jsx
+++ Client/src/components/ContactForm.jsx
@@ -18,11 +18,14 @@ export default function ContactForm() {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:5000/send-email", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(formData),
-      });
+      const response = await fetch(
+        "https://happybalance.turboot.com/send-email",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(formData),
+        }
+      );
 
       if (response.ok) {
         setStatus("Email succesvol verzonden!");
